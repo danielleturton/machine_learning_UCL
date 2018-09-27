c5035aabdc93a0a01b383ac72ee0a3524e8e51c6
danielleturton <danielleturtonbb@gmail.com>
2018-09-27 12:41:22 +0100

    Week 3 script

---
+++ week3/dogs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+greyhounds = 500
+labs = 500
+
+grey_height = 28
+lab_height = 24
+
+grey_height = 28 + 4 * np.random.randn(greyhounds)
+lab_height = 24 + 4 * np.random.randn(labs)
+
+plt.hist([grey_height, lab_height], stacked = True, color = ['r', 'b'])
+plt.show()
