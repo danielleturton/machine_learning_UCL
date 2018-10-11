be1e02891300f637556109c69bd31bf84aa488c8
danielleturton <danielleturtonbb@gmail.com>
2018-10-11 11:41:42 +0100

    week 5 work

---
+++ week5/iris_pipeline_random.py
@@ -0,0 +1,32 @@
+import random
+
+class ScrappyKNN():
+    def fit(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+
+    def predict(self, X_test):
+        predictions = []
+        for row in X_test:
+            label = random.choice(self.y_train)
+            predictions.append(label)
+        return predictions
+
+from sklearn import datasets
+iris = datasets.load_iris()
+
+X = iris.data
+y = iris.target
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .5) #use half data for testing
+
+my_classifier = ScrappyKNN()
+
+my_classifier.fit(X_train, y_train)
+
+predictions = my_classifier.predict(X_test)
+print predictions
+
+from sklearn.metrics import accuracy_score
+print accuracy_score(y_test, predictions)
